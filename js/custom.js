9877cb7d3bfff4447acfdead86c6544104d9123e
juidebnath10 <118449402+juidebnath10@users.noreply.github.com>
2023-02-08 16:59:58 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$('.slick_slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots:true,
+    speed: 800,
+    infinite: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<i class="fas fa-angle-left left-arrow">',
+    NextArrow: '<i class="fas fa-angle-right right-arrow">',
+  });
